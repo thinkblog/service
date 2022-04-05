9d4ac8c98d853bd6a71b7a9003277fabb7830c57
醉月思 <tim@thinkmoon.cn>
2022-04-05 23:53:37 +0800

    1. 增加权限标签 2. 限制请求类型

--- src/main/java/cn/thinkmoon/blog/modules/attachment/web/AttachmentController.java
+++ src/main/java/cn/thinkmoon/blog/modules/attachment/web/AttachmentController.java
@@ -1,8 +1,11 @@
 package cn.thinkmoon.blog.modules.attachment.web;
 
+import cn.thinkmoon.blog.core.annotation.Permission;
+import cn.thinkmoon.blog.core.enums.PermissionTag;
 import cn.thinkmoon.blog.modules.attachment.service.AttachmentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +17,8 @@ public class AttachmentController {
     @Autowired
     private AttachmentService attachmentService;
 
-    @RequestMapping(value = "/upload_code")
+    @Permission(permissionTag = PermissionTag.ADMIN)
+    @GetMapping(value = "/upload_code")
     public String getUploadCode(){
         return attachmentService.generateUploadCode();
     }
