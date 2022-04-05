edeb6adbccf483dba23915286c1e2ab0afbfe2aa
醉月思 <tim@thinkmoon.cn>
2022-04-05 23:37:10 +0800

    增加七牛云静态类

--- src/main/java/cn/thinkmoon/blog/modules/attachment/web/AttachmentController.java
+++ src/main/java/cn/thinkmoon/blog/modules/attachment/web/AttachmentController.java
@@ -1,6 +1,8 @@
 package cn.thinkmoon.blog.modules.attachment.web;
 
+import cn.thinkmoon.blog.modules.attachment.service.AttachmentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/attachment")
 public class AttachmentController {
 
+    @Autowired
+    private AttachmentService attachmentService;
+
     @RequestMapping(value = "/upload_code")
     public String getUploadCode(){
-
-        return "";
+        return attachmentService.generateUploadCode();
     }
 }
