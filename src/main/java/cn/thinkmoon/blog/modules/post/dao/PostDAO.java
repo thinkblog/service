e2b7b053cfd6a6bc04e4b5352f1a0d093b7253a2
醉月思 <tim@thinkmoon.cn>
2022-04-10 18:48:35 +0800

    fix: 分页列表改为子查询

--- src/main/java/cn/thinkmoon/blog/modules/post/dao/PostDAO.java
+++ src/main/java/cn/thinkmoon/blog/modules/post/dao/PostDAO.java
@@ -1,9 +1,13 @@
 package cn.thinkmoon.blog.modules.post.dao;
 
+import cn.thinkmoon.blog.modules.post.pojo.FieldsPO;
 import cn.thinkmoon.blog.modules.post.pojo.PostPO;
+import cn.thinkmoon.blog.modules.post.pojo.TagPo;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+
+import java.util.List;
 
 /**
  * <p>
@@ -14,6 +18,10 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
  * @since 2020-12-12
  */
 public interface PostDAO extends BaseMapper<PostPO> {
+    List<TagPo> selectTag(int cid);
+
+    List<FieldsPO> selectFields(int cid);
+
     IPage<PostPO> selectPage(Page<?> page, int mid, String keyword);
 
     PostPO getDetail(String cid);
